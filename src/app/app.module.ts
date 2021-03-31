1c2ed2ca7d38d135540645eba80bb4232a103ecf
RODOLFOM-coder <rsilvamiranda91@gmail.com>
2021-03-30 21:04:54 -0300

    Criando as pastas pagina/contato/home/lista e menu no APP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { FormsModule} from '@angular/forms'
+import { FormsModule} from '@angular/forms';
+import { ContatoComponent } from './pagina/contato/contato.component';
+import { HomeComponent } from './pagina/home/home.component';
+import { MenuComponent } from './pagina/menu/menu.component';
+import { ListaComponent } from './pagina/lista/lista.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContatoComponent,
+    HomeComponent,
+    MenuComponent,
+    ListaComponent
   ],
   imports: [
     BrowserModule,
